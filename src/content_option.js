7e9289e56a6fe38c94c3abb8c17f65efa117d39f
S1moonC <simon.j.cada@gmail.com>
2024-01-10 12:05:49 +0100

    portfolio init

--- src/content_option.js
+++ src/content_option.js
@@ -1,18 +1,18 @@
-const logotext = "JOHN";
+const logotext = "Čada";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Čada Produkce",
+    description: "Jsem Šimon Čada data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "Šimon Čada",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Party DJ",
+        second: "Zvukař",
+        third: "Produkce",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    your_img_url: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/182023918_5474967112578669_2144950163301087455_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=0bb214&_nc_ohc=fKyhRr8YZNIAX8HhpH2&_nc_ht=scontent-prg1-1.xx&oh=00_AfBogapSVe1WMPKxjbus3mJJ5PnDvPSH4ofX6YpRFHuZhw&oe=65C5DB59",
 };
 
 const dataabout = {
@@ -20,14 +20,14 @@ const dataabout = {
     aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Dazed and Confused",
+        where: "Blues-Rock",
+        link: "https://www.youtube.com/watch?v=u4J0USObOVk&ab_channel=Kostkaradio",
     },
     {
         jobtitle: "Designer of week",
         where: "Jamalya",
-        date: "2019",
+        link: "",
     },
     {
         jobtitle: "Designer of week",
@@ -73,82 +73,81 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/182571818_5474967445911969_7960391918174028724_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=0bb214&_nc_ohc=dYWw27WUE6gAX-OH-vh&_nc_ht=scontent-prg1-1.xx&oh=00_AfBx-vesAPRBjAeIaRZC6u2Z3RkILAuONWjyXhizQr5-Yw&oe=65C5F7E2",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/181275032_5474967232578657_5045811638055584851_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0bb214&_nc_ohc=n6G2CfLWnycAX9zW2Kn&_nc_ht=scontent-prg1-1.xx&oh=00_AfATvdB_eXzpgocg0EdyGzZqrnEJVaygZrySnGXUNXUGRw&oe=65C5FD58",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/181630358_5474965579245489_7055027249754830196_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=0bb214&_nc_ohc=gipYGqNT9eEAX-MV24m&_nc_ht=scontent-prg1-1.xx&oh=00_AfDjQ2TSJm9qSVs1euzpXXK8oRmkrt_yfebqm36PMHYNdg&oe=65C5FD73",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/181091957_5474962065912507_3116963655511302343_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=0bb214&_nc_ohc=_uBQCEb91N0AX-u5HJX&_nc_ht=scontent-prg1-1.xx&oh=00_AfA5neOysQr44Hh_5x_fsyk-Sd9O8_itRyGW-mqwLez-Gw&oe=65C5F3C9",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/181075371_5474963522579028_6504465423180573464_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=0bb214&_nc_ohc=mJTv9NkMavYAX9I0z4F&_nc_oc=AQnrWvEMHM4OLCBuyfL2qlqFftGlrdXp-iUX2bpEWpdLaPOZCYK3nxvXT3ZLuZb2OjeSOEeWj1Ht_gsMn51E_m0S&_nc_ht=scontent-prg1-1.xx&oh=00_AfCl9bAl3tScpmmyQmU2Ev6vEOTVdrrz7E9VtR936Maz1g&oe=65C5EBB3",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/182188776_5474966152578765_5753389485602359306_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=0bb214&_nc_ohc=nU_jVX32XIsAX_gSq7V&_nc_ht=scontent-prg1-1.xx&oh=00_AfDiKKAAMarj60fnngGPn9xFKgx1blIHYVpebAzsZ0zJAA&oe=65C5FBC5",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/182165255_5474966419245405_258788705371805082_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=0bb214&_nc_ohc=QqEhrPGqj58AX_G4vuR&_nc_ht=scontent-prg1-1.xx&oh=00_AfC7WMglcXUNEmNiAMjm69tugasGUiocOpYxbZVS5BLBEw&oe=65C60079",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/182248624_5474964509245596_4107917645519598526_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=0bb214&_nc_ohc=hIKDwqvMfowAX9a6PcL&_nc_oc=AQm-Y58rs6wbQvzGrVExlJido1lcf1xsOwa85h0zjbLF7bpCkrMsIz6WQqkD3vMBctvD8XEXtScchTUqwJG7Elqd&_nc_ht=scontent-prg1-1.xx&oh=00_AfC3ITWpFVbs-j14i9WZyhAPDngBZeajHIaI6Qs1NiBdpw&oe=65C5D4BE",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/182408950_5474963975912316_7429122347740468602_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=0bb214&_nc_ohc=rvHmObH6JskAX-uCDdL&_nc_ht=scontent-prg1-1.xx&oh=00_AfBFX4lOo1B_O1p7HmptvCCxv2KYmXhZulYioJ2ZoTNmDg&oe=65C5F312",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/550/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/180858541_5474961295912584_2293443497286010662_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=0bb214&_nc_ohc=X1CMibM-p9oAX_QwL7_&_nc_ht=scontent-prg1-1.xx&oh=00_AfA5_3DKbqKsweeeCB87Xax-rDWNX0PqxlKl6pL0_Vo7Dw&oe=65C5DAFF",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/180496621_5474961809245866_129129595298516311_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0bb214&_nc_ohc=mj0CzPkdfgwAX_VTt0k&_nc_ht=scontent-prg1-1.xx&oh=00_AfAhdFnEgYlnj1Gtt5esz9fsdDamtyJuRXK_Ppw3Qj5JKw&oe=65C5D543",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
+        img: "https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/180881022_5474966032578777_5839376829780661615_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=0bb214&_nc_ohc=_lbhh1AtBCYAX9N5yoF&_nc_ht=scontent-prg1-1.xx&oh=00_AfCHkNTfVsvjZDjO3UYj-FrBkQzVh-kAQ5SEzFo4urNUdw&oe=65C5F4B3",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
+    YOUR_EMAIL: "cadaprodukce@info.cz",
+    YOUR_FONE: "+420 777 222 771",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
     YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
+    github: "https://github.com/S1moonC",
+    facebook: "https://www.facebook.com/profile.php?id=100008208610713",
     linkedin: "https://linkedin.com",
     twitter: "https://twitter.com",
 };
